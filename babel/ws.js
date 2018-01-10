0616e98a6062f7289d542f6c8023027b936ba8dc
nojaja <free.riccia@gmail.com>
2018-01-10 09:31:27 +0900

    wsのログの出力を減らした

--- babel/ws.js
+++ babel/ws.js
@@ -1,8 +1,29 @@
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      console.log(event);
-      return response || fetch(event.request);
+      return response || fetch(event.request).then(function(_response) {
+        console.log("fetch:"+event.request.url);
+        return _response;
+      });
     })
   );
 });
+
+/*
+const STATIC_CACHE_KEY = '1';
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(function(_response) {
+        console.log("cache.put:"+event.request.url);
+        caches.open(STATIC_CACHE_KEY).then(function(cache) {
+          console.log("_response:"+_response);
+          cache.put(event.request, _response.clone());
+        });
+        return _response;
+      });
+    })
+  );
+});
+*/
