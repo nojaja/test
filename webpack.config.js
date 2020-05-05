0e0399b022eafbbece3a58be31729b96c56926d1
nojaja <free.riccia@gmail.com>
2020-05-05 17:18:45 +0000

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const CopyFilePlugin = require('copy-webpack-plugin')
 const WriteFilePlugin = require('write-file-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'development' : 'production',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: dist,
