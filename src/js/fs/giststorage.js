0239a8d2d0727c30bf58d0df422f01f223edc8e2
nojaja <free.riccia@gmail.com>
2020-05-23 18:45:14 +0900

    fix git token取得処理

--- src/js/fs/giststorage.js
+++ src/js/fs/giststorage.js
@@ -10,16 +10,14 @@ export class GistStorage {
         let _getToken = async () => {
             return new Promise(resolve => {
                 if (this.tokens == null) {
-                    $.UIkit.modal.prompt('<p>Gist</p><br><p><a href="https://github.com/settings/tokens">Personal access tokens</a>:</p>', '', (newtoken) => {
-                        console.log('Confirmed.'+newtoken);
-                        this.token = newtoken;
-                        resolve(true)
+                    $.UIkit.modal.prompt('<p>Gist Authentication</p><br><p><a href="https://github.com/settings/tokens" target="_blank">Personal access tokens</a>:</p>', '', (newtoken) => {
+                        this.tokens = newtoken;
+                        resolve(this.tokens)
                     }, () => {
-                        console.log('newtoken Rejected.')
-                        resolve(false)
+                        resolve(null)
                     })
                 } else {
-                    resolve(false)
+                    resolve(this.tokens)
                 }
             })
         }
@@ -27,63 +25,54 @@ export class GistStorage {
     }
 
     //プロジェクト一覧取得
-    loadList (callback) {
-        $.ajax({
-            url: gisturl,
-            type: 'GET',
-            beforeSend: (xhr) => {
-                // <p>Gist</p><br><p><a href="https://github.com/settings/tokens">Personal access tokens</a>:</p>
-                // if (this.tokens == null) this.token = prompt("<p>Gist</p><br><p><a href='https://github.com/settings/tokens'>Personal access tokens</a>:</p>", "")
-                this.getToken()
-                xhr.setRequestHeader("Authorization", "token " + this.tokens)
-            }
-        }).done((response) => {
-            let list = { rows: response }
-            localStorage.setItem('gittoken', this.tokens)
-            return (callback) ? callback(list, "gist") : list
-        }).fail((jqXHR, textStatus, errorThrown) => {
-            // エラーの場合処理
-            this.tokens = null
-        });
+    async loadList (callback) {
+        if(await this.getToken()){
+            $.ajax({
+                url: gisturl,
+                type: 'GET',
+                beforeSend: (xhr) => {
+                    // <p>Gist</p><br><p><a href="https://github.com/settings/tokens">Personal access tokens</a>:</p>
+                    // if (this.tokens == null) this.token = prompt("<p>Gist</p><br><p><a href='https://github.com/settings/tokens'>Personal access tokens</a>:</p>", "")
+                    xhr.setRequestHeader("Authorization", "token " + this.tokens)
+                }
+            }).done((response) => {
+                let list = { rows: response }
+                localStorage.setItem('gittoken', this.tokens)
+                return (callback) ? callback(list, "gist") : list
+            }).fail((jqXHR, textStatus, errorThrown) => {
+                // エラーの場合処理
+                this.tokens = null
+            })
+        }
     }
 
-    saveDraft(fileContainer) {
-        $.UIkit.notify("Share Gist..", { status: 'success', timeout: 1000 });
-
-        let sendType = "POST";
-        let _gisturl = gisturl;
-        if (fileContainer.getGistId()) {
-            sendType = "PATCH";
-            _gisturl = gisturl + "/" + fileContainer.getGistId();
+    async saveDraft(fileContainer) {
+        if(await this.getToken()){
+            $.UIkit.notify("Share Gist..", { status: 'success', timeout: 1000 });
+    
+            let sendType = "POST";
+            let _gisturl = gisturl;
+            if (fileContainer.getGistId()) {
+                sendType = "PATCH";
+                _gisturl = gisturl + "/" + fileContainer.getGistId();
+            }
+            $.ajax({
+                url: _gisturl,
+                type: sendType,
+                dataType: 'json',
+                beforeSend: (xhr) => {
+                    xhr.setRequestHeader("Authorization", "token " + this.tokens);
+                },
+                data: fileContainer.getGistJsonData()
+            }).done((response) => {
+                $.UIkit.notify("complete!", { status: 'success', timeout: 1000 })
+                localStorage.setItem('gittoken', this.tokens)
+                fileContainer.setGistId(response.id)
+            }).fail((jqXHR, textStatus, errorThrown) => {
+                console.warn("gist save error", errorThrown);
+                $.UIkit.notify("error..", { status: 'error', timeout: 1000 });
+            })
         }
-        $.ajax({
-            url: _gisturl,
-            type: sendType,
-            dataType: 'json',
-            beforeSend: (xhr) => {
-                this.getToken()
-                xhr.setRequestHeader("Authorization", "token " + this.tokens);
-            },
-            data: fileContainer.getGistJsonData()
-        }).done((response) => {
-            $.UIkit.notify("complete!", { status: 'success', timeout: 1000 })
-            localStorage.setItem('gittoken', this.tokens)
-
-            fileContainer.setGistId(response.id)
-
-            //TODO ここにGASへの登録処理を追加する
-            //URL https://script.google.com/macros/s/AKfycbzjYobwi6G61HPTeiUue67PlOHvnsj2E_SFgzi-CVoV/dev?p=/uid/reactcomponent/ ファイル名.gist&contents=gistID
-
-
-            /*
-            $.getJSON(gasUrl+filename+ ".gist&contents="+e.id+"&callback=?",  { t: '1' }, function(json){
-                console.log(json);
-            });
-            */
-        }).fail((jqXHR, textStatus, errorThrown) => {
-            console.warn("gist save error", errorThrown);
-            $.UIkit.notify("error..", { status: 'error', timeout: 1000 });
-        });
     }
 
     loadDraft(fileContainer, url, callback) {
