7d40aab5b5e5b57b58cd1b5dbed81f6aa64ecd6a
nojaja <free.riccia@gmail.com>
2020-05-31 17:46:28 +0900

    fix sass

--- src/js/compiler/sasscompiler.js
+++ src/js/compiler/sasscompiler.js
@@ -1,16 +1,26 @@
 import FileData from '../model/FileData.js'
 
 export class SassCompiler {
-    constructor(fileContainer) {
+    constructor(fileContainer, logger) {
         this.fileContainer = fileContainer
+        this.logger = logger
+        this.error = logger.getError()
+        this.info = logger.getInfo()
     }
 
     async compile(targetFile, outpath) {
         //todo let data = targetFile.getEditorData()
         let filename = targetFile.getFilename()
+        console.log(filename)
+        let ext = filename.substr(filename.lastIndexOf("."));
         filename = filename.substr(0, filename.lastIndexOf("."));
-
-        Sass.options('defaults');
+        console.log(filename)
+        console.log(ext)
+        if(ext==".sass"){
+            Sass.options({indentedSyntax: true});
+        } else {
+            Sass.options({indentedSyntax: false});
+        }
         // Sass.compile(data.source.model.getValue().trim(), async function(result) {
         Sass.compile(targetFile.getContent().trim(), async (result) => {
             if (result.status == 0) {
@@ -26,6 +36,7 @@ export class SassCompiler {
                 //todo if(data)data.decorations = data.model.deltaDecorations(data.decorations, { range: new monaco.Range(1,1,1,1), options : { } });
                 //await this.cachesLogic.saveCache(filename+'.css',result.text,'text/css; charset=UTF-8');
             } else {
+                this.error(result.message + ' line:'+result.line+' column:'+result.column)
                 UIkit.notify(result.message, { status: 'warning', timeout: 1000 });
                 //エラー箇所の表示
                 //todo if(data)data.decorations = data.model.deltaDecorations(data.decorations, [
